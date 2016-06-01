b23f2f8f87642ba55e8c96efda8d0d3155abc086
Nadiah95 <twinz_angel94@yahoo.com>
2016-06-02 00:57:39 +0800

    Update hover-image.js

--- hover-image.js
+++ hover-image.js
@@ -29,7 +29,19 @@ $(document).ready(function() {
 			$(this).find("span").stop()
 			.animate({top:'0%'},{duration:350, easing:'easeOutQuart'});
 		})
+		.live('mouseleave',function(){$(document).ready(function() { 
+	$(".lightbox-image").append("<span></span>");
+	
+	$('.lightbox-image')
+		.live('mouseenter',function(){
+			$(this).find("span").stop()
+			.animate({top:'0%'},{duration:350, easing:'easeOutQuart'});
+		})
 		.live('mouseleave',function(){
+			$(this).find("span").stop()
+			.animate({top:'-100%'},{duration:350, easing:'easeOutQuart'});
+	});
+});
 			$(this).find("span").stop()
 			.animate({top:'-100%'},{duration:350, easing:'easeOutQuart'});
 	});
